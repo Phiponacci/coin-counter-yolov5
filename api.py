82e85c58be954c216c1eb18ff5fec315f4f74e89
Phiponacci <leo.phibonacci@gmail.com>
2022-01-21 23:16:46 +0100

    final release

--- api.py
+++ api.py
@@ -4,17 +4,11 @@ import numpy as np
 from flask import Flask, request, Response, jsonify
 from PIL import Image
 import cv2
-from firebase_api import *
 import io
 import base64
 
 app = Flask(__name__)
 
-
-def numpy_to_binary(arr):
-    is_success, buffer = cv2.imencode(".jpg", arr)
-    io_buf = io.BytesIO(buffer)
-    return io_buf
 
 
 @app.route('/count', methods=['POST'])
@@ -39,14 +33,9 @@ def count():
             "label": label,
             "position": [x1, y1, x2, y2]
         }
-        
-        im = cv2.rectangle(im, (x1, y1), (x2, y2), color=(255,0,0), thickness=2)
-        im = cv2.putText(im, label, (x1, y1), fontFace=FONT_HERSHEY_SIMPLEX, fontScale=0.8, color=(0,255,0), thickness=2)
         results.append(result)
     
-    img = numpy_to_binary(im)
-    saved_result = save_results(uid, img, results)
-    return jsonify({"results": results, "image": saved_result['photo']})
+    return jsonify({"results": results})
 
 
 
